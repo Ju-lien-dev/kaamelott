9ae281135105bc716f62decda0d6882a0a3559b4
Julien <baylejulien@hotmail.fr>
2025-07-03 16:36:42 +0200

    test responsive

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,26 +1,21 @@
-'use client';
+"use client";
 
 export default function Header() {
- 
   return (
-        <header className="w-full bg-white ">
+    <header className="max-h-[500px] w-full bg-white ">
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
-        <div className="w-1/3 flex justify-start">
-       
-        </div>
+        <div className="w-1/3 flex justify-start"></div>
 
-        <div className="w-1/3 flex justify-center">
+        <div className="xs:w-1/3 flex justify-center">
           <img
-            src="/assets/kaamelott_circle.png" 
+            src="/assets/kaamelott_circle.png"
             alt="Logo"
-            className="size-fit"
+            className="object-contain min-w-[150px] min-h-[150px] max-w-full"
           />
         </div>
-        <div className="w-1/3 flex justify-end">
 
-        </div>
+        <div className="md:w-1/3 flex justify-end"></div>
       </div>
     </header>
-   
-  )
+  );
 }
